752557723a1dedcc37fdbeef9a6623d4a0bd3fdf
Felipe Quintella <felipe@quintella.com>
2022-05-13 15:33:32 -0300

    INC

---
+++ src/extras/authentication/functions.php
@@ -0,0 +1,6 @@
+<?php
+
+// Check if the user has multi factor authentication
+function enabled_auth($user){
+    return 1;
+}
--- src/extras/authentication/login.php
+++ src/extras/authentication/login.php
@@ -1,5 +1,8 @@
 <?php
 require_once "vendor/autoload.php";
+
+//define("TOOLKIT_PATH", 'vendor/onelogin/php-saml/');
+//require_once('vendor/onelogin/php-saml/_toolkit_loader.php');
 require_once "settings.php";
 
 $auth = new \OneLogin\Saml2\Auth($settings);
