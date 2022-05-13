5ab66e5e799209f295df690bd19b20fd8e99a7d5
Felipe Quintella <felipe@quintella.com>
2022-05-13 17:10:49 -0300

    INC

--- src/extras/authentication/functions.php
+++ src/extras/authentication/functions.php
@@ -4,9 +4,10 @@ require_once __DIR__.'/vendor/analog/analog/lib/Analog.php';
 
 use Analog\Analog;
 
-Analog::handler (Analog\Handler\Stderr::init ());
 
-Analog::log ('TESTE LOG');
+Analog::handler (\Analog\Handler\Syslog::init ('SR-ADDONS', 'www-data'));
+
+Analog::log ('Error message', Analog::WARNING);
 
 // Check if the user has multi factor authentication
 function enabled_auth($user){
