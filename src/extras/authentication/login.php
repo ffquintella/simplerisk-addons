e3f5aec081286781d2a38332f3dc75b935e9ddd6
Felipe Quintella <felipe@quintella.com>
2022-05-13 19:15:33 -0300

    INC

--- src/extras/authentication/login.php
+++ src/extras/authentication/login.php
@@ -1,4 +1,6 @@
 <?php
+session_start(); 
+
 require_once "functions.php";
 
 //define("TOOLKIT_PATH", 'vendor/onelogin/php-saml/');
@@ -8,4 +10,26 @@ require_once "settings.php";
 $auth = new \OneLogin\Saml2\Auth($settings);
 
 
-var_dump($auth);
+if (isset($_SESSION) && isset($_SESSION['AuthNRequestID'])) {
+    $requestID = $_SESSION['AuthNRequestID'];
+} else {
+    $requestID = null;
+}
+
+$auth->processResponse($requestID);
+unset($_SESSION['AuthNRequestID']);
+
+$errors = $auth->getErrors();
+
+if (!empty($errors)) {
+    echo '<p>', implode(', ', $errors), '</p>';
+    exit();
+}
+
+if (!$auth->isAuthenticated()) {
+    echo "<p>Not authenticated</p>";
+    exit();
+}
+
+
+//var_dump($auth);
