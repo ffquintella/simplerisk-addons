c1be71604dd129c24e2c18111de3cc893c47d736
Felipe Quintella <felipe@quintella.com>
2022-05-13 19:27:48 -0300

    INC

--- src/extras/authentication/login.php
+++ src/extras/authentication/login.php
@@ -1,35 +1,37 @@
 <?php
-session_start(); 
-
 require_once "functions.php";
 
 //define("TOOLKIT_PATH", 'vendor/onelogin/php-saml/');
 //require_once('vendor/onelogin/php-saml/_toolkit_loader.php');
 require_once "settings.php";
 
-$auth = new \OneLogin\Saml2\Auth($settings);
+//$auth = new \OneLogin\Saml2\Auth($settings);
 
+session_start();
+$needsAuth = empty($_SESSION['samlUserdata']);
 
-if (isset($_SESSION) && isset($_SESSION['AuthNRequestID'])) {
-    $requestID = $_SESSION['AuthNRequestID'];
-} else {
-    $requestID = null;
+if ($needsAuth) {
+    // put SAML settings into an array to avoid placing files in the
+    // composer vendor/ directories 
+    $samlsettings = array(/*...config goes here...*/);
+    
+    $auth = new \OneLogin\Saml2\Auth($settings);
+
+    if (!empty($_REQUEST['SAMLResponse']) && !empty($_REQUEST['RelayState'])) {
+        $auth->processResponse(null);
+        $errors = $auth->getErrors();
+        if (empty($errors)) {
+            // user has authenticated successfully
+            $needsAuth = false;
+            $_SESSION['samlUserdata'] = $auth->getAttributes();
+        }
+    }
+
+    if ($needsAuth) {
+        $auth->login();
+    }
 }
 
-$auth->processResponse($requestID);
-unset($_SESSION['AuthNRequestID']);
-
-$errors = $auth->getErrors();
-
-if (!empty($errors)) {
-    echo '<p>', implode(', ', $errors), '</p>';
-    exit();
-}
-
-if (!$auth->isAuthenticated()) {
-    echo "<p>Not authenticated</p>";
-    exit();
-}
 
 
 //var_dump($auth);
