05c06adaccd9fd56b223fca1242bd381d82b149c
Felipe Quintella <felipe@quintella.com>
2022-05-13 16:18:17 -0300

    INC

--- src/extras/authentication/functions.php
+++ src/extras/authentication/functions.php
@@ -1,4 +1,16 @@
 <?php
+require_once "vendor/autoload.php";
+
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('/var/log/apache2/simplerisk-addons.log', Level::Info));
+
+// add records to the log
+$log->info('Enabling SimpleRiskAddons');
 
 // Check if the user has multi factor authentication
 function enabled_auth($user){
--- src/extras/authentication/login.php
+++ src/extras/authentication/login.php
@@ -1,5 +1,5 @@
 <?php
-require_once "vendor/autoload.php";
+require_once "functions.php"
 
 //define("TOOLKIT_PATH", 'vendor/onelogin/php-saml/');
 //require_once('vendor/onelogin/php-saml/_toolkit_loader.php');
