5059cd423e94f601842a8b1ced52702a02d5c754
Felipe Quintella <felipe@quintella.com>
2022-05-14 11:48:29 -0300

    INC

--- src/extras/authentication/index.php
+++ src/extras/authentication/index.php
@@ -5,8 +5,12 @@ function display_authentication(){
 
 global $escaper, $lang;
 ?>
+    <link rel="stylesheet" href="css/auth.css">
     <form name="authentication_settings" method="post" action="">
         <input class="hidden-checkbox" readonly type="checkbox" id="enabled" name="enabled"<?php if (get_setting('custom_auth') == 'true') echo " checked" ?> /><label for="enabled">&nbsp;&nbsp;<?php echo $escaper->escapeHtml($lang['Enabled']); ?></label>
+        <div class="title">
+            Service Provider Info:
+        </div>
         <br\><br\>
         <input type="submit" value="<?php echo $escaper->escapeHtml($lang['Update']); ?>" name="authentication_settings_update" />
     </form>
