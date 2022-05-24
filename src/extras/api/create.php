64efad110c96f9af82d086e74e4653a58b35e7f1
Felipe F Quintella <felipe@quintella.com>
2022-05-24 15:54:38 -0300

    INC

---
+++ src/extras/api/create.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once __DIR__.'/vendor/analog/analog/lib/Analog.php';
+require_once __DIR__.'/../../includes/functions.php';
+require_once __DIR__.'/../../includes/api.php';
+
+// Include the SimpleRisk language file
+require_once(language_file());
+// Include Laminas Escaper for HTML Output Encoding
+$escaper = new Laminas\Escaper\Escaper('utf-8');
+
+use Analog\Analog;
+
+Analog::handler (\Analog\Handler\FirePHP::init ());
+
+Analog::log ('SimpleRisk API Addon Activated', Analog::INFO);
+
+if (is_authenticated())
+{
+
+    Analog::log ('Creating API Key', Analog::INFO);
+
+    if($_POST["api-name"])
+
+    $api_name = isset($_POST['api-name']) ? $_POST['api-name'] : '';
+    $api_value = isset($_POST['api-value']) ? $_POST['api-value'] : '';
+
+
+    if(empty($api_name)) {
+         Analog::log ('Invalid empty api-name', Analog::ERROR);
+         $_SESSION["validation-error"] = true;
+         $_SESSION["validation-message"] = 'Invalid empty api-name';
+         header("Location: /admin/api.php");
+         exit();    
+    }
+    if(empty($api_value)) {
+        Analog::log ('Invalid empty api-value', Analog::ERROR);
+        $_SESSION["validation-error"] = true;
+        $_SESSION["validation-message"] = 'Invalid empty api-value';
+        header("Location: /admin/api.php");
+        exit();    
+    }
+    if(strlen($api_value) < 12) {
+        Analog::log ('API VALUE too short', Analog::ERROR);
+        $_SESSION["validation-error"] = true;
+        $_SESSION["validation-message"] = 'API VALUE too short';
+        header("Location: /admin/api.php");
+        exit();    
+    }
+
+    header("Location: /admin/api.php");
+    exit();
+
+}else{
+    Analog::log ('USER NOT AUTHENTICATED', Analog::ERROR);
+    echo "NOT AUTHENTICATED";
+}
+
--- src/extras/api/index.php
+++ src/extras/api/index.php
@@ -4,7 +4,6 @@ require_once "functions.php";
 
 define('API_EXTRA_VERSION', '1.0.1');
 
-
 function display_api(){
     global $escaper, $lang;
 
@@ -16,6 +15,14 @@ function display_api(){
     <div class="info-text"> To use this addon you should use the 
         URL: https://yoursite/extras/api/api.php/records/endpoint. 
         All endpoints can be listet at https://yoursite/extras/api/api.php/openapi </div>
+<?php
+
+if($_SESSION["validation-error"] == true){
+    $_SESSION["validation-error"] = false;
+    echo "<div class='warning'>" . $escaper->escapeHtml($_SESSION["validation-message"]) . "</div>";
+}
+
+?>
 
 
     <div class="sheader">API KEYS</div>
@@ -31,8 +38,17 @@ function display_api(){
 ?>
     </table>
 
+    <div class="sheader">Create API Key</div>
+    <form name="api_keys" method="post" action="/extras/api/create.php">
+        <table>
+            <tr><td>Name</td><td>Value</td></tr>
+            <tr><td><input type="texto" name="api-name" value="" /></td><td><input type="texto" name="api-value" value="" /></td></tr>
+            <tr><td colspan="2"><input type="submit" name="deactivate" value="<?php echo $escaper->escapeHtml($lang['Send']); ?>" name="send" /></td></tr>
+        </table>
+    </form>
+
     <form name="api_settings" method="post" action="">
-    <input type="submit" name="deactivate" value="<?php echo $escaper->escapeHtml($lang['Deactivate']); ?>" name="deactivate" />
+        <input type="submit" name="deactivate" value="<?php echo $escaper->escapeHtml($lang['Deactivate']); ?>" name="deactivate" />
     </form>
 
 <?php
