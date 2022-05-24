713a87dcfa0a0f7323cdbcc9ab809b6aa14a66de
Felipe F Quintella <felipe@quintella.com>
2022-05-24 12:20:59 -0300

    INC

--- src/extras/api/functions.php
+++ src/extras/api/functions.php
@@ -4,6 +4,10 @@ require_once __DIR__."/vendor/autoload.php";
 require_once __DIR__.'/vendor/analog/analog/lib/Analog.php';
 require_once __DIR__.'/../../includes/functions.php';
 
+// Include the SimpleRisk language file
+require_once(language_file());
+// Include Laminas Escaper for HTML Output Encoding
+$escaper = new Laminas\Escaper\Escaper('utf-8');
 
 use Analog\Analog;
 
@@ -24,6 +28,29 @@ function enable_api_extra(){
 
         // Store the value in the database
         $stmt = $db->prepare("INSERT INTO settings VALUES('api','true');");
+        $stmt->execute();
+    }
+
+
+
+    // Close the database connection
+    db_close($db);
+    return;
+}
+
+function disable_api_extra(){
+    // Open the database connection
+    $db = db_open();
+
+    if (!empty(get_setting('api'))){
+        // Store the file in the database
+        $stmt = $db->prepare("UPDATE settings SET VALUE='false' WHERE NAME='api';");
+        $stmt->execute();
+
+    }else{
+
+        // Store the value in the database
+        $stmt = $db->prepare("INSERT INTO settings VALUES('api','false');");
         $stmt->execute();
     }
 
--- src/extras/api/index.php
+++ src/extras/api/index.php
@@ -1,3 +1,20 @@
 <?php
 
 require_once "functions.php";
+
+define('API_EXTRA_VERSION', '1.0.1');
+
+
+function display_api(){
+    global $escaper, $lang;
+
+?>
+   <link rel="stylesheet" href="/extras/api/css/api.css">
+    <form name="api_settings" method="post" action="">
+        
+
+        <input type="submit" name="deactivate" value="<?php echo $escaper->escapeHtml($lang['Deactivate']); ?>" name="deactivate" />
+        </form>
+
+<?php
+}
