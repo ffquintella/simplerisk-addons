1d01f863aa5e7de2e7546b0ee1979ad31429f107
Felipe F Quintella <felipe@quintella.com>
2022-06-09 10:29:22 -0300

    INC

---
+++ scripts/dev/reset-notification-db.php
@@ -0,0 +1,26 @@
+<?php
+
+
+require_once __DIR__.'/../../includes/functions.php';
+require_once __DIR__.'/../../includes/governance.php';
+require_once __DIR__.'/../../includes/mail.php';
+
+
+// THIS SCRIPT SHOULD NOT BE DISTRIBUTED TO PRODUCTION ENVIROMENTS
+
+
+echo "Reseting notification database .... <br\>" ;
+
+$db = db_open();
+
+
+// Store the file in the database
+$stmt = $db->prepare("UPDATE settings SET VALUE='false' WHERE NAME='notifications';");
+$stmt->execute();
+
+$stmt = $db->prepare("DROP TABLE addons_notification_messages;");
+$stmt->execute();
+
+echo "Reseting finished. <br\>" ;
+
+db_close($db);
