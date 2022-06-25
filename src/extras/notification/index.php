7eaf05166195b7648fb8cd4e791b1df722dcc041
Felipe Quintella <felipe@quintella.com>
2022-06-24 22:39:53 -0300

    uodate 5

--- src/extras/notification/index.php
+++ src/extras/notification/index.php
@@ -132,6 +132,22 @@ foreach (get_notification_variables() as $key => $value){ ?>
         </tr>
         <tr>
             <?php 
+            if(get_notification_message_status("document_update") != "enabled"){
+                $bt_img = "off-button.png";
+                $action = "enable.php";
+            }else {
+                $bt_img = "on-button.png";
+                $action = "disable.php";
+            }
+            ?>
+        <td><?php echo $escaper->escapeHtml($lang_not['Document update']); ?><a href='/extras/notification/<?php echo $action; ?>?id=13'><img class='btimg' src='/extras/notification/imgs/<?php echo $bt_img; ?>' /></a></td>
+        </tr>
+        <tr>
+        <td><textarea class="not_text" id="documentupdate" name="documentupdate" rows="4"  width="100%"><?php echo get_notification_message("document_update");?></textarea></td>
+        
+        </tr>
+        <tr>
+            <?php 
             if(get_notification_message_status("new_audit_comment") != "enabled"){
                 $bt_img = "off-button.png";
                 $action = "enable.php";
