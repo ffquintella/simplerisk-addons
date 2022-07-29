8c2d90438b5434f440bb71d474e40642b8fdac12
Felipe F Quintella <felipe@quintella.com>
2022-07-29 11:46:09 -0300

    Scafold

---
+++ src/net-extras/DAL/DAL.cs
@@ -0,0 +1,17 @@
+﻿namespace DAL;
+
+using Microsoft.Extensions.Configuration;
+
+public class DAL
+{
+    // requires using Microsoft.Extensions.Configuration;
+    private readonly IConfiguration Configuration;
+    private string ConnectionString;
+    
+    public DAL(IConfiguration configuration)
+    {
+        Configuration = configuration;
+        ConnectionString = Configuration["Dabatase:ConnectionString"];
+
+    }
+}
