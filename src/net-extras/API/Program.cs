569fb184a12c5897f61a25a0dfd5ef83ca9cfad9
Felipe F Quintella <felipe@quintella.com>
2022-07-29 19:10:35 -0300

    INC

--- src/net-extras/API/Program.cs
+++ src/net-extras/API/Program.cs
@@ -1,3 +1,5 @@
+using Saml2.Authentication.Core.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,28 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Saml2.Authentication.Core
+builder.Services.Configure<Saml2Configuration>(builder.Configuration.GetSection("Saml2"));
+
+builder.Services.AddSaml();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = "saml2";
+        options.DefaultChallengeScheme = "saml2";
+    })
+    .AddCookie("saml2.cookies", options =>
+    {
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SameSite = SameSiteMode.None;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+    })
+    .AddSaml("saml2", "saml2", options =>
+    {
+        options.SignInScheme = "saml2.cookies";
+        options.IdentityProviderName = "stubidp.sustainsys";
+    });
 
 var app = builder.Build();
 
@@ -18,6 +42,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
