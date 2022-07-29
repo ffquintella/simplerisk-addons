5e755b209eb80a32cc516dc48e26615ab257b390
Felipe F Quintella <felipe@quintella.com>
2022-07-29 14:38:25 -0300

    INC

--- src/net-extras/ConsoleTest/Program.cs
+++ src/net-extras/ConsoleTest/Program.cs
@@ -3,6 +3,7 @@
 using System.Net.Mime;
 using Microsoft.Extensions.Configuration;
 using Pomelo.EntityFrameworkCore.MySql.Query.ExpressionTranslators.Internal;
+using DAL;
 
 Console.WriteLine("This is a test application!");
 
@@ -17,5 +18,9 @@ var config = configuration.Build();
 var connectionString = config["Database:ConnectionString"];
 
 Console.WriteLine("ConnectionString:" + connectionString);
+
+var dalManager = new DALManager(config);
+
+var dbContext = dalManager.GetContext();
 
 Environment.Exit(-1);
