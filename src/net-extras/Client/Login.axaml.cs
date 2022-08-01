67960ce8acf6fac741e88776327363cd5e22b512
Felipe F Quintella <felipe@quintella.com>
2022-07-31 23:28:04 -0300

    INC

---
+++ src/net-extras/Client/Login.axaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Client;
+
+public partial class Login : Window
+{
+    public Login()
+    {
+        InitializeComponent();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
