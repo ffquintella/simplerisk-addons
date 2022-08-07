d9f12a52ac4df53d1d973a3be7a6adba82df2a05
Felipe F Quintella <felipe@quintella.com>
2022-08-07 01:15:14 -0300

    INC

--- src/net-extras/GUIClient/Configuration/MutableConfiguration.cs
+++ src/net-extras/GUIClient/Configuration/MutableConfiguration.cs
@@ -3,6 +3,6 @@
 public class MutableConfiguration
 {
     public int ID { get; set; }
-    public string Name { get; set; }
-    public string Value { get; set; }
+    public string Name { get; set; } = "";
+    public string? Value { get; set; } 
 }
