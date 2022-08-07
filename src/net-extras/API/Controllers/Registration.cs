18be30fd71920a420d8f20929501a0ab7460fe62
Felipe F Quintella <felipe@quintella.com>
2022-08-07 19:17:04 -0300

    INC

---
+++ src/net-extras/API/Controllers/Registration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Model.Registration;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Registration : ControllerBase
+{
+    [AllowAnonymous]
+    [HttpPost]
+    [Route("/")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status412PreconditionFailed, Type = typeof(string))]
+    public ActionResult<string> Register([FromBody] RegistrationRequest request)
+    {
+        string hashCode = String.Format("{0:X}", request.Id.GetHashCode());
+        return Ok(hashCode);
+        return this.StatusCode(StatusCodes.Status412PreconditionFailed, "Already exists");
+    }
+}
