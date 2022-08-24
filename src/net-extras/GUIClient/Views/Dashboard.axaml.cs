8bd06f68a4a258881aec9f4b0f0ce96177ef4398
Felipe <felipe@quintella.com>
2022-08-24 14:29:55 -0300

    dash

---
+++ src/net-extras/GUIClient/Views/Dashboard.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace GUIClient.Views;
+
+public partial class Dashboard : UserControl
+{
+    public Dashboard()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
