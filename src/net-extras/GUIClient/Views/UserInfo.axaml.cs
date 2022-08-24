eba17458a79b4dfacda95fef9e9d8b724da40364
Felipe <felipe@quintella.com>
2022-08-24 15:37:55 -0300

    INC

---
+++ src/net-extras/GUIClient/Views/UserInfo.axaml.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using GUIClient.ViewModels;
+
+namespace GUIClient.Views;
+
+public partial class UserInfo : Window
+{
+    public UserInfo()
+    {
+
+        DataContext = new UserInfoViewModel();
+        
+        InitializeComponent();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
