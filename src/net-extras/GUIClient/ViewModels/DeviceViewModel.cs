174d66acdf74fd6c59b3858303e327e2252339d7
Felipe <felipe@quintella.com>
2022-08-25 09:42:26 -0300

    INC

--- src/net-extras/GUIClient/ViewModels/DeviceViewModel.cs
+++ src/net-extras/GUIClient/ViewModels/DeviceViewModel.cs
@@ -1,6 +1,17 @@
-﻿namespace GUIClient.ViewModels;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GUIClient.ViewModels;
 
 public class DeviceViewModel: ViewModelBase
 {
+    public List<String> TestData { get; set; } = new List<string>();
+
+    public DeviceViewModel()
+    {
+        TestData.Add("test1");
+        TestData.Add("teste2");
+    }
     
 }
