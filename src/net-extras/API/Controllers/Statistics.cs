e8eda340ab78364e206b97b3832018a403243f44
Felipe F Quintella <felipe@quintella.com>
2022-08-26 19:41:51 -0300

    INC

--- src/net-extras/API/Controllers/Statistics.cs
+++ src/net-extras/API/Controllers/Statistics.cs
@@ -15,4 +15,13 @@ public class Statistics : ControllerBase
     {
         return new List<string> { "RisksOverTime", "SecurityControls" };
     }
+    
+    [HttpGet]
+    [Route("RisksOverTime")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<string>))]
+    public ActionResult<Dictionary<string,double>> GetRisksOverTime()
+    {
+        return new Dictionary<string,double> { { "Jan", 1.0 }, { "Fev", 2.0 }, { "Mar", 8.0 } };
+    }
+    
 }
