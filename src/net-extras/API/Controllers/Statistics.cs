e0c0ce07e3ba8bf085de2a954437673d31320697
Felipe F Quintella <felipe@quintella.com>
2022-08-27 12:33:41 -0300

    INC

--- src/net-extras/API/Controllers/Statistics.cs
+++ src/net-extras/API/Controllers/Statistics.cs
@@ -1,55 +1,70 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DAL;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Model.Statistics;
+using ILogger = Serilog.ILogger;
 
 namespace API.Controllers;
 
 [Authorize(Policy = "RequireValidUser")]
 [ApiController]
 [Route("[controller]")]
-public class Statistics : ControllerBase
+public class Statistics : ApiBaseController
 {
+    private readonly DALManager _dalManager;
+    
+    public Statistics(ILogger logger, DALManager dalManager) : base(logger)
+    {
+        _dalManager = dalManager;
+    }
+    
+
     [HttpGet]
     [Route("")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<string>))]
     public ActionResult<List<string>> ListAvaliable()
     {
+        Logger.Information("Listing avaliable statistics");
         return new List<string> { "RisksOverTime", "SecurityControls" };
     }
     
     [HttpGet]
     [Route("RisksOverTime")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<string>))]
-    public ActionResult<List<RisksOnDay>> GetRisksOverTime()
+    public ActionResult<List<RisksOnDay>> GetRisksOverTime([FromQuery]int daysSpan = 30)
     {
-        return new List<RisksOnDay>
+
+        var firstDay = DateTime.Now.Subtract(TimeSpan.FromDays(daysSpan));
+        
+        var srDbContext = _dalManager.GetContext();
+        var risks = srDbContext.Risks.Where(risk => risk.SubmissionDate > firstDay).ToList();
+        
+        var result = new List<RisksOnDay>();
+
+        var computingDay = firstDay;
+
+        while (computingDay < DateTime.Now)
         {
-            new RisksOnDay
+            var risksSelected = risks.Where(rsk => rsk.SubmissionDate.Date == computingDay.Date && rsk.Status != "Closed").ToList();
+
+            var riskOnDay = new RisksOnDay
             {
-                Day = new DateTime(2022,1,1),
-                RisksCreated = 5,
-                TotalRiskValue = 22.5f
-            },
-            new RisksOnDay
-            {
-                Day = new DateTime(2022,1,2),
-                RisksCreated = 1,
-                TotalRiskValue = 2.5f
-            },
-            new RisksOnDay
-            {
-                Day = new DateTime(2022,1,3),
-                RisksCreated = 15,
-                TotalRiskValue = 45.5f
-            },
-            new RisksOnDay
-            {
-                Day = new DateTime(2022,1,4),
-                RisksCreated = 8,
-                TotalRiskValue = 32.3f
-            },
+                Day = computingDay,
+                RisksCreated = 0,
+                TotalRiskValue = 0
+            };
             
-        };
+            foreach (var risk in risksSelected)
+            {
+                riskOnDay.RisksCreated++;
+                
+            }
+            result.Add(riskOnDay);
+            computingDay = computingDay.AddDays(1);
+        }
+
+        return result;
+
     }
     
 }
