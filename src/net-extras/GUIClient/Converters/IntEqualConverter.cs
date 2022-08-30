bc1695299fd338ab3cbd924191201ff1d4946910
Felipe <felipe@quintella.com>
2022-08-30 17:14:19 -0300

    INC

---
+++ src/net-extras/GUIClient/Converters/IntEqualConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Microsoft.Extensions.Localization;
+
+namespace GUIClient.Converters;
+
+public class IntEqualConverter: IValueConverter
+{
+    public static readonly IntEqualConverter Instance = new();
+
+    public object? Convert(
+                            object? value, 
+                            Type targetType, 
+                            object? parameter, 
+                            CultureInfo culture)
+    {
+        if (value is int sourceText && parameter is int compareText
+                                       && targetType.IsAssignableTo(typeof(bool)))
+        {
+
+            if (sourceText == compareText)
+            {
+                return true;
+            }
+            return false;
+            
+        }
+        // converter used for the wrong type
+        return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
+    }
+    
+    public object ConvertBack( object? value, Type targetType, object? parameter, CultureInfo culture )
+    {
+        throw new NotSupportedException();
+    }
+
+}
