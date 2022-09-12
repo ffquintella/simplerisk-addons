98df75d4e2a0c5e9e3ef9ad0fb88d03c3eb1d06d
Felipe Quintella <felipe@quintella.com>
2022-09-11 22:21:32 -0300

    INC

--- scripts/puppet/manifests/base.pp
+++ scripts/puppet/manifests/base.pp
@@ -1,7 +1,4 @@
 
-package{'zip':
-  ensure => present
-}
 
 file{'/srnet/SRNET-ConsoleClient/ConsoleClient':
   mode => '755'
@@ -15,13 +12,6 @@ file{'/srnet/SRNET-GUIClient-lin/GUIClient':
   mode => '755'
 }
 
-
-# Cleaning unused packages to decrease image size
-exec {'erase installer':
-  path  => '/bin:/sbin:/usr/bin:/usr/sbin',
-  command => 'rm -rf /tmp/*; rm -rf /opt/staging/*'
-} ->
-
 exec {'erase cache':
   path  => '/bin:/sbin:/usr/bin:/usr/sbin',
   command => 'rm -rf /var/cache/*'
@@ -32,10 +22,3 @@ exec {'erase logs':
 }
 
 
-package {'openssh': ensure => absent }
-package {'openssh-clients': ensure => absent }
-package {'openssh-server': ensure => absent }
-package {'rhn-check': ensure => absent }
-package {'rhn-client-tools': ensure => absent }
-package {'rhn-setup': ensure => absent }
-package {'rhnlib': ensure => absent }
--- scripts/puppet/manifests/start.pp
+++ scripts/puppet/manifests/start.pp
@@ -1,10 +1,9 @@
 
 
-exec {'Coping Configs':
-  path    => '/bin:/sbin:/usr/bin:/usr/sbin',
-  command => "echo \"Coping configs ...\"; cp -r /opt/jira-config/* ${real_appdir}/conf; chown -R jira:jira ${real_appdir}/conf ",
-  creates => "${real_appdir}/conf/server.xml"
-} ->
+
+
+
+
 # Starting jira
 exec {'Starting Jira':
   path  => '/bin:/sbin:/usr/bin:/usr/sbin',
