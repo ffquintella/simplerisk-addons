e21815a9beccec0c1eb786b121f03cd29c1e92ad
Felipe F Quintella <felipe@quintella.com>
2022-09-29 11:24:47 -0300

    fix dbcreation

--- src/net-extras/DAL/Scripts/DB/DB-SQL-2.sql
+++ src/net-extras/DAL/Scripts/DB/DB-SQL-2.sql
@@ -1,5 +1,5 @@
 ﻿ALTER TABLE `simplerisk`.`addons_client_registration`
-    ADD INDEX `Ind_ExternalID`(`ExternalId`) USING BTREE;
+    ADD INDEX (`ExternalId`) USING BTREE;
 
 ALTER TABLE `simplerisk`.`addons_client_registration`
     MODIFY COLUMN `Id` int(11) NOT NULL AUTO_INCREMENT FIRST;
