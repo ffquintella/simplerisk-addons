52c3141cde20d59aa44d503d81cea412f880f541
Felipe F Quintella <felipe@quintella.com>
2023-01-18 15:21:55 -0300

    INC

--- src/net-extras/GUIClient/Services/IRisksService.cs
+++ src/net-extras/GUIClient/Services/IRisksService.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+using DAL.Entities;
+
 namespace GUIClient.Services;
 
 public interface IRisksService
 {
-    
+    public List<Risk> GetAllRisks();
 }
--- src/net-extras/GUIClient/Services/RisksService.cs
+++ src/net-extras/GUIClient/Services/RisksService.cs
@@ -1,6 +1,14 @@
+using System.Collections.Generic;
+using DAL.Entities;
+
 namespace GUIClient.Services;
 
 public class RisksService: IRisksService
 {
-    
+    public List<Risk> GetAllRisks()
+    {
+        var result = new List<Risk>();
+
+        return result;
+    }
 }
