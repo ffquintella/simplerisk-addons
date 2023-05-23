5c3c04553ecab52ff3ed3d4265ebb999db1879cb
Felipe F Quiontella <felipe@quintella.com>
2023-05-23 14:07:04 -0300

    INC

--- src/net-extras/GUIClient/App.axaml.cs
+++ src/net-extras/GUIClient/App.axaml.cs
@@ -58,6 +58,8 @@ namespace GUIClient
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
                 desktop.MainWindow = new MainWindow();
+                desktop.MainWindow.Width = 1000;
+                desktop.MainWindow.Height = 700;
             }
 
            
