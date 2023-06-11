01bba8602fe346a9b04b1a0b942fee5a6bdc3470
Felipe F Quintella <felipe@quintella.com>
2023-06-11 20:19:46 -0300

    INC

--- src/net-extras/GUIClient/ViewModels/RiskViewModel.cs
+++ src/net-extras/GUIClient/ViewModels/RiskViewModel.cs
@@ -184,6 +184,7 @@ public class RiskViewModel: ViewModelBase
     private void ExecuteReloadRisk()
     {
         AllRisks = new ObservableCollection<Risk>(_risksService.GetAllRisks());
+        RiskFilter = "";
     }
 
     private void Initialize()
